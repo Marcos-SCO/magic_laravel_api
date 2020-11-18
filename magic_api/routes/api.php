be0de556da146364f5a213c4f6068c3582119bd9
Marcos-SCO <marcos_sco@outlook.com>
2020-11-18 10:20:39 -0300

    Staff Members routes

--- magic_api/routes/api.php
+++ magic_api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\StaffMembersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,12 +24,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // Route::apiResource('funcionarioTipos','api/StaffTypesController');
 
 // Pega os tipos de funcionário
-Route::get('/tiposfuncionarios',[StaffTypesController::class, 'index']);
+Route::get('/tiposfuncionarios', [StaffTypesController::class, 'index']);
 // Pega um tipo de funcionário por meio do Id
-Route::get('tiposfuncionarios/{id}',[StaffTypesController::class, 'show']);
+Route::get('tiposfuncionarios/{id}', [StaffTypesController::class, 'show']);
 // Cria um funcionário passando os valores com form-data
-Route::post('/tiposfuncionarios',[StaffTypesController::class, 'store']);
+Route::post('/tiposfuncionarios', [StaffTypesController::class, 'store']);
 // Atualiza um tipo de funcionário passando os valores com form-data
-Route::put('/tiposfuncionarios',[StaffTypesController::class, 'update']);
+Route::put('/tiposfuncionarios', [StaffTypesController::class, 'update']);
 // Deleta um tipo de funcionário
-Route::delete('/tiposfuncionarios',[StaffTypesController::class, 'destroy']);
+Route::delete('/tiposfuncionarios', [StaffTypesController::class, 'destroy']);
+
+
+// ------------------------------------------------------
+// -------------------- Staff Members -------------------
+// ------------------------------------------------------
+
+// Pega todos funcionários
+Route::get('/funcionarios', [StaffMembersController::class, 'index']);
+// Pega um funcionário por meio do Id
+Route::get('/funcionario/{id}', [StaffMembersController::class, 'show']);
+// Criar um novo funcionário
+Route::post('/funcionarios', [StaffMembersController::class, 'store']);
+// Atualiza um tipo de funcionário passando os valores com form-data
+Route::put('/funcionarios', [StaffMembersController::class, 'update']);
+// Deleta um tipo de funcionário
+Route::delete('/funcionarios', [StaffMembersController::class, 'destroy']);
