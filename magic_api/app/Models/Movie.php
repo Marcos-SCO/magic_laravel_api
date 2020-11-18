d6b445f5426fe62afa44a55034a2efc4f7987288
Marcos-SCO <marcos_sco@outlook.com>
2020-11-18 12:34:31 -0300

    Movies Model

---
+++ magic_api/app/Models/Movie.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'movie_category_id',
+        'movie_description'
+    ];
+}
